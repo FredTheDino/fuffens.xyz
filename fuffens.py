96d1bcbd288060c4f0b62322c979b1cc5284d415
Edvard Thörnros <edvard.thornros@gmail.com>
2021-04-27 00:16:14 +0200

    A humble begining

--- fuffens.py
+++ fuffens.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
-    help(app.run)
-    app.run('0.0.0.0', 5000)
+    app.run('0.0.0.0', 5000, static="static")
